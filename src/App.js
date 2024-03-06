ba099ff566cf963357d927ec6d9c00f8590e41e5
M. Shoaib Anwar <mshoaibanwar2001@gmail.com>
2024-03-07 01:48:04 +0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
+import LeftNav from './components/LeftNav';
+import Pizzas from './components/Pizzas'
+import AddPost from './components/AddPost'
+import EditPost from './components/EditPost'
+import Categories from './components/Categories'
+import Sauces from './components/Sauces';
+import Orders from './components/Orders';
+import Users from './components/Users';
+import Home from './components/Home';
+import Veggies from './components/Veggies';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Tab from 'react-bootstrap/Tab';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row App m-0 p-0">
+      <Tab.Container id="left-tabs-example" defaultActiveKey="home">
+                <BrowserRouter>
+                  <LeftNav/>
+                  <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="items" element={<Pizzas />} />
+                        <Route path="add" element={<AddPost />} />
+                        <Route path="edit" element={<EditPost />} />
+                        <Route path="categories" element={<Categories />} />
+                        <Route path="sauces" element={<Sauces />} />
+                        <Route path="veggies" element={<Veggies />} />
+                        <Route path="Orders" element={<Orders />} />
+                        <Route path="users" element={<Users />} />
+                    </Routes>
+                </BrowserRouter>
+      </Tab.Container>
     </div>
   );
 }
